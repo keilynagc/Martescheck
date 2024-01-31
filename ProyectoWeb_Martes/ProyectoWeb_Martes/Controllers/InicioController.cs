51e3a4ff520e927692fc4b6c124c68f9bc49564f
ecalvo92 <edcalvo92@hotmail.com>
2024-01-30 18:27:52 -0600

    Commit Inicial

---
+++ ProyectoWeb_Martes/ProyectoWeb_Martes/Controllers/InicioController.cs
@@ -0,0 +1,26 @@
+﻿using ProyectoWeb_Martes.Models;
+using System.Web.Mvc;
+
+namespace ProyectoWeb_Martes.Controllers
+{
+    public class InicioController : Controller
+    {
+        UsuarioModel modelo = new UsuarioModel();
+
+        public ActionResult IniciarSesion()
+        {
+            return View();
+        }
+
+        public ActionResult RegistrarUsuario()
+        {
+            return View();
+        }
+
+        public ActionResult PantallaPrincipal()
+        {
+            return View();
+        }
+
+    }
+}
