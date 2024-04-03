e6e1fa1541641787d8f119921250cc1c1024a1bb
ecalvo92 <edcalvo92@hotmail.com>
2024-04-02 19:53:19 -0600

    Primera parte semana 12

--- ProyectoApi_Martes/ProyectoApi_Martes/Controllers/ProductoController.cs
+++ ProyectoApi_Martes/ProyectoApi_Martes/Controllers/ProductoController.cs
@@ -1,6 +1,7 @@
 ﻿using ProyectoApi_Martes.Entidades;
 using ProyectoApi_Martes.Models;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
 
---
+++ ProyectoWeb_Martes/ProyectoWeb_Martes/Controllers/CarritoController.cs
@@ -0,0 +1,20 @@
+﻿using ProyectoWeb_Martes.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProyectoWeb_Martes.Controllers
+{
+    [FiltroSeguridad]
+    [OutputCache(NoStore = true, VaryByParam = "*", Duration = 0)]
+    public class CarritoController : Controller
+    {
+        [HttpGet]
+        public ActionResult AgregarCarrito(long idProducto, int cantProducto)
+        {
+            return Json("OK", JsonRequestBehavior.AllowGet);
+        }
+    }
+}
